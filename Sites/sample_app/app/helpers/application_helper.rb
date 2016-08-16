4d86530e3b88bcba5c92ba42b0367a0b11aae34f
yovan <yovan.juggoo@chb-technologies.ch>
2016-08-16 09:16:42 +0400

    Added a title helper

--- Sites/sample_app/app/helpers/application_helper.rb
+++ Sites/sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# Return a title on a per-page basis.
+	def title
+		base_title = "Ruby on Rails Sample App"
+		if @title.nil? #By default @title being an instance variable is nil
+			base_title #methods return the last line evaluated
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
