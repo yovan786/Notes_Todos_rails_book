b24780eeaaeb58d0ab88a1167495aa8a777f6c4e
Yovan Juggoo <yovan.juggoo@gmail.com>
2016-08-27 19:47:56 +0400

    Create a new blog app and som creation notes in Creating_app.txt

---
+++ Sites/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c85b547b76c7c3c9638c3b281128420e362920fb5e6f5c1d53458b5bfb97316229d7c838acf6320b486fe126a9d3f8ccf5ef3eb31edd926a8205432def543908'
