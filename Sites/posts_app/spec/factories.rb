d3ed39cd5e77d710daffbb00f5aeaf1b17f18b0a
yovan <yovan.juggoo@chb-technologies.ch>
2016-09-07 16:49:32 +0400

    Improved the user show page and added a Gravatar

--- Sites/posts_app/spec/factories.rb
+++ Sites/posts_app/spec/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |user|
-	user.name "Nicolas Pierre"
-	user.email "nicolas@chb.com"
+	user.name "Yovan Juggoo"
+	user.email "yovan.juggoo@gmail.com"
 	user.password "foobar"
 	user.password_confirmation "foobar"
 end
